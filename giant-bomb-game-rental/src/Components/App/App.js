e00afc9084dbf4cacf9f9d72121b065b097c6711
Courtney Olesee <court.olson1@gmail.com>
2020-01-24 18:02:28 -0600

    material UI-ed search and game cards

--- giant-bomb-game-rental/src/Components/App/App.js
+++ giant-bomb-game-rental/src/Components/App/App.js
@@ -1,38 +1,84 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
-import GameCard from '../GameCard/GameCard'
+import GameCard from '../GameCard/GameCard';
+// Material UI Imports
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+// Material UI Styles
+const styles = theme => ({
+  button: {
+    margin: theme.spacing(),
+  },
+  // input: {
+  //   display: 'none',
+  // },
+  textField: {
+    marginLeft: theme.spacing(),
+    marginRight: theme.spacing(),
+  },
+});
 
 class App extends Component {
 
   // changes with user input
   state = {
-    search: ''
+    search: '',
+    lastSearch: ''
   }
 
   // function to handles the change in the input
   handleChange = (event) => {
     this.setState({
       search: event.target.value
-    })
+    });
   }
 
   // function to dispatch (to the root saga listening for it) the payload (user search input)
-  performSearch = ()=> {
+  performSearch = () => {
     this.props.dispatch({type: 'SEARCH_GAMES', payload: this.state.search})
+    this.setState({
+      lastSearch: this.state.search
+    });
+    this.setState({
+      search: ''
+    });
   }
+
 
   // renders App
   render() {
+
+    const { classes } = this.props;
+
     return (
       <div className="App">
         <header className="App-header">
           <h1>Gravie Software Engineer Challenge</h1>
             <p>Giant Bomb API</p>
         </header>
+
         <div className="search">
-          <input placeholder="search" type="text" onChange={(event)=>this.handleChange(event, 'search')} value={this.state.search}/>
-          <button onClick={this.performSearch}>search</button>
+          <h3>Search for your favorite games to rent!</h3>
+          <TextField
+          id="outlined-name"
+          label="Game Keywords"
+          className={classes.textField}
+          value={this.state.search}
+          onChange={(event)=>this.handleChange(event, 'search')}
+          margin="normal"
+          variant="outlined"
+          /> <br/>
+          <Button variant="contained" className={classes.button}
+          onClick={this.performSearch}>
+            Search
+          </Button>
+        </div>
+        <div>
+          <p>Showing results for: {this.state.lastSearch}</p><br/>
         </div>
         <div className="gameSection">
           {this.props.game.map( (item,i)=> {     
@@ -44,4 +90,10 @@ class App extends Component {
   }
 }
 
-export default connect(reduxState=>({game: reduxState.searchReducer}))(App);
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default connect(reduxState=>({game: reduxState.searchReducer}))(withStyles(styles)(App));
+
--- giant-bomb-game-rental/src/Components/GameCard/GameCard.js
+++ giant-bomb-game-rental/src/Components/GameCard/GameCard.js
@@ -1,20 +1,66 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+// Material UI Imports
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
+
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
 
 class GameCard extends Component {
 
-
-
   render () {
+
+    const { classes } = this.props;
+
     return (
       <div className="gameCard">
-         <img src={this.props.item.image} alt={this.props.item.name}/>
-         <p>{this.props.item.name}</p>
+         {/* <img src={this.props.item.image} alt={this.props.item.name}/>
+         <p>{this.props.item.name}</p> */}
+         <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={this.props.item.image}
+              title={this.props.item.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {this.props.item.name}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Rent
+            </Button>
+          </CardActions>
+        </Card>
       </div>
     )
   }
 }
 
 
-export default connect()(GameCard);
+GameCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(GameCard);
+
+
+
