9f9da40cfd4804ae1e69195713b59ac172d7f4df
Courtney Olesee <court.olson1@gmail.com>
2020-01-26 10:58:39 -0600

    gravie logo added in header, css on header and checkoutBtn

--- giant-bomb-game-rental/src/Components/App/App.js
+++ giant-bomb-game-rental/src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import Results from '../Results/Results';
+import Logo from './logo.png'
 // Material UI Imports
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -58,33 +59,40 @@ class App extends Component {
 
     return (
       <div className="App">
+          <div className="checkoutBtn">
+            <Button 
+                variant="contained" 
+                className={classes.button}
+                onClick={this.goToCheckout}>
+                Ready to Checkout?
+              </Button>
+            </div>
         <header className="App-header">
-          <h1>Gravie Software Engineer Challenge</h1>
-            <p>Giant Bomb API Game Rental</p>
+          <div className="title">
+            <img className="logo" src={Logo} alt="Gravie Logo" />
+            <h1> Software Engineer Challenge</h1>
+          </div>
         </header>
         <div className="search">
-          <h3>Search for games to rent!</h3>
-            <TextField
-              id="outlined-name"
-              label="Game Keywords"
-              className={classes.textField}
-              value={this.state.search}
-              onChange={(event)=>this.handleChange(event, 'search')}
-              margin="normal"
-              variant="outlined"
-            /> <br/>
-            <Button 
-              variant="contained" 
-              className={classes.button}
-              onClick={this.performSearch}>
-                Search
-            </Button><br/>
-            <Button 
-              variant="contained" 
-              className={classes.button}
-              onClick={this.goToCheckout}>
-              Ready to Checkout?
-            </Button>
+          <p>Giant Bomb API Game Rental</p>
+          <div>
+            <h3>Search for games to rent!</h3>
+              <TextField
+                id="outlined-name"
+                label="Game Keywords"
+                className={classes.textField}
+                value={this.state.search}
+                onChange={(event)=>this.handleChange(event, 'search')}
+                margin="normal"
+                variant="outlined"
+              /> <br/>
+              <Button 
+                variant="contained" 
+                className={classes.button}
+                onClick={this.performSearch}>
+                  Search
+              </Button>
+          </div>
         </div>
 
         <div>
